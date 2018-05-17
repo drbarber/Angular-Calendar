830dd24274324b1116918edc39e61a0c1f15e819
drbarber <sgosman506@gmail.com>
2018-05-16 21:43:16 -0400

    making small progress on calendar generating

--- Angular-Calendar/src/app/app.component.ts
+++ Angular-Calendar/src/app/app.component.ts
@@ -7,14 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
  startDate;
- endDate: Date = new Date();
+ endDate;
+ fullEndDate: Date = new Date();
+ fullStartDate: Date = new Date();
  days: number;
 
+  months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  daysOfWeek = ["Su","Mo","Tu","We","Th","Fr","Sa"];
+  calendarDays: any[];
 
-  addDays(){
-    console.log(this.startDate.day)
-    this.endDate.setDate(this.startDate.day + this.days);
+   addDays(){
 
-    console.log(this.endDate);
+    this.fullEndDate.setFullYear(this.startDate.year);
+    this.fullEndDate.setMonth(this.startDate.month);
+    this.fullEndDate.setDate(this.startDate.day + this.days);
+    this.fullStartDate.setFullYear(this.startDate.year);
+    this.fullStartDate.setMonth(this.startDate.month);
+    this.fullStartDate.setDate(this.startDate.day);
+
+    this.endDate = {year: this.fullEndDate.getFullYear(), month: this.fullEndDate.getMonth(), day: this.fullEndDate.getDate()};
+
+    this.generateCalendar();
   }
+
+
+  generateCalendar(){
+    var startWeekday  = (this.fullStartDate.getDay() + 4) % 7;
+    this.calendarDays = [];
+    
+    for (var i = 0; i < startWeekday; i++){
+      this.calendarDays.push({valid: "no", dayType:  i == 6||i == 0 ? "Weekend": "Weekday" });
+    }
+
+
+    console.log(startWeekday);
+  }
+ 
 }
